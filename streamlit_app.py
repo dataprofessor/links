081457aabfb384db27a26ed96d733de892e8aab7
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-06-03 22:42:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,13 @@
 import streamlit as st
+from PIL import Image
+import webbrowser
 
+with open("style.css") as f:
+    st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+
+st.title('Chanin Nantasenamat, Ph.D.')
+
+st.write('Developer Advocate, Content Creator and ex-Professor with an interest in Data Science and Bioinformatics')
+
+if st.button('YouTube channel'):
+    webbrowser.open_new_tab('https://youtube.com/dataprofessor')
