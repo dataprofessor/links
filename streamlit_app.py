0ee6f62baac0d264d02ac79dc7ce275ee4f80d8f
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-06-03 23:11:33 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ with open("style.css") as f:
     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 
-col1, col2, col3 = st.columns(3)
+col1, col2, col3 = st.columns([0.1, 5, 0.1])
 col2.image(Image.open('dp.png'), width=150)
 
 st.title('Chanin Nantasenamat, Ph.D.')
