cfbb46769b9df33e61aea5971635b3ab96f870ee
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-06-04 16:05:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ st.header('Chanin Nantasenamat, Ph.D.')
 st.info('Developer Advocate, Content Creator and ex-Professor with an interest in Data Science and Bioinformatics')
 
 st.markdown("""
-<a href='https://www.youtube.com/dataprofessor' class="btn btn-outline-primary btn-lg btn-block" type="button" aria-pressed="true">Data Professor YouTube channel</a>
+<a href='https://www.youtube.com/dataprofessor' class="btn btn-outline-primary btn-lg btn-block" type="button" aria-pressed="true"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-youtube" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/></svg> Data Professor YouTube channel</a>
 <a href='https://www.youtube.com/codingprofessor' class="btn btn-outline-primary btn-lg btn-block" type="button" aria-pressed="true">Coding Professor YouTube channel</a>
 <a href='https://data-professor.medium.com/' class="btn btn-outline-primary btn-lg btn-block" type="button" aria-pressed="true">Read my Blogs</a>
 <a href='https://twitter.com/thedataprof' class="btn btn-outline-primary btn-lg btn-block" type="button" aria-pressed="true">Follow me on Twitter</a>
