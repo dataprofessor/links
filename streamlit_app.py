93e2369380430a8e059649708a212a119da3751c
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-06-07 19:56:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,16 @@ def st_button(icon, url, label):
                 {label}
             </a>
         </p>'''
-    if icon == '':
+    elif icon == 'medium':
+        button_code = f'''
+        <p>
+            <a href={url} class="btn btn-outline-primary btn-lg btn-block" type="button" aria-pressed="true">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-medium" viewBox="0 0 16 16">
+                    <path d="M9.025 8c0 2.485-2.02 4.5-4.513 4.5A4.506 4.506 0 0 1 0 8c0-2.486 2.02-4.5 4.512-4.5A4.506 4.506 0 0 1 9.025 8zm4.95 0c0 2.34-1.01 4.236-2.256 4.236-1.246 0-2.256-1.897-2.256-4.236 0-2.34 1.01-4.236 2.256-4.236 1.246 0 2.256 1.897 2.256 4.236zM16 8c0 2.096-.355 3.795-.794 3.795-.438 0-.793-1.7-.793-3.795 0-2.096.355-3.795.794-3.795.438 0 .793 1.699.793 3.795z"/>
+                </svg>
+            </a>
+        </p>'''
+    elif icon == '':
         button_code = f'''
         <p>
             <a href={url} class="btn btn-outline-primary btn-lg btn-block" type="button" aria-pressed="true">
@@ -35,7 +44,7 @@ def st_button(icon, url, label):
 
 st_button('youtube', 'https://youtube.com/dataprofessor', 'Data Professor YouTube channel')
 st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel')
-st_button('', 'https://data-professor.medium.com/', 'Read my Blogs')
+st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs')
 
 st.markdown("""
 <a href='https://twitter.com/thedataprof' class="btn btn-outline-primary btn-lg btn-block" type="button" aria-pressed="true">Follow me on Twitter</a>
