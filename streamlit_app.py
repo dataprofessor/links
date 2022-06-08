c12a8ad4b1767077edd5ee90936172f0f0f6c9c4
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-06-08 20:46:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,6 +54,16 @@ def st_button(icon, url, label):
                 {label}
             </a>
         </p>'''
+    if icon == 'newsletter':
+        button_code = f'''
+        <p>
+            <a href={url} class="btn btn-outline-primary btn-lg btn-block" type="button" aria-pressed="true">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+                    <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
+                </svg>
+                {label}
+            </a>
+        </p>'''
     elif icon == '':
         button_code = f'''
         <p>
@@ -68,8 +78,8 @@ st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor Yo
 st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs')
 st_button('twitter', 'https://twitter.com/thedataprof/', 'Follow me on Twitter')
 st_button('linkedin', 'https://www.linkedin.com/in/chanin-nantasenamat/', 'Follow me on LinkedIn')
+st_button('newsletter', 'https://sendfox.com/dataprofessor/', 'Sign up for my Newsletter')
 
 st.markdown("""
-<a href='https://sendfox.com/dataprofessor' class="btn btn-outline-primary btn-lg btn-block" type="button" aria-pressed="true">Sign up for my Newsletter</a>
 <a href='https://www.buymeacoffee.com/dataprofessor' class="btn btn-outline-primary btn-lg btn-block" type="button" aria-pressed="true">Buy me a Coffee</a>
 """, unsafe_allow_html=True)
