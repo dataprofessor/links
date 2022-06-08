09fddf59ab6668ba280c03de5474f658111d775a
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-06-08 20:41:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,16 @@ def st_button(icon, url, label):
             {label}
         </a>
         </p>'''
+    if icon == 'linkedin':
+        button_code = f'''
+        <p>
+            <a href={url} class="btn btn-outline-primary btn-lg btn-block" type="button" aria-pressed="true">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+                    <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+                </svg>
+                {label}
+            </a>
+        </p>''' 
     elif icon == 'medium':
         button_code = f'''
         <p>
@@ -56,10 +66,10 @@ def st_button(icon, url, label):
 st_button('youtube', 'https://youtube.com/dataprofessor', 'Data Professor YouTube channel')
 st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel')
 st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs')
-st_button('twitter', 'https://twitter.com/thedataprof', 'Follow me on Twitter')
+st_button('twitter', 'https://twitter.com/thedataprof/', 'Follow me on Twitter')
+st_button('linkedin', 'https://www.linkedin.com/in/chanin-nantasenamat/', 'Follow me on LinkedIn')
 
 st.markdown("""
-<a href='https://www.linkedin.com/in/chanin-nantasenamat/' class="btn btn-outline-primary btn-lg btn-block" type="button" aria-pressed="true">Follow me on LinkedIn</a>
 <a href='https://sendfox.com/dataprofessor' class="btn btn-outline-primary btn-lg btn-block" type="button" aria-pressed="true">Sign up for my Newsletter</a>
 <a href='https://www.buymeacoffee.com/dataprofessor' class="btn btn-outline-primary btn-lg btn-block" type="button" aria-pressed="true">Buy me a Coffee</a>
 """, unsafe_allow_html=True)
