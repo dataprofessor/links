9579c96bce0a21e666335a2f809c4a84516cad96
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-06-27 19:54:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,17 +3,12 @@ from st_functions import st_button
 from PIL import Image
 import webbrowser
 
-with open("style.css") as f:
-    st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
-st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
-
 col1, col2, col3 = st.columns(3)
 col2.image(Image.open('dp.png'))
 
 st.header('Chanin Nantasenamat, Ph.D.')
 
 st.info('Developer Advocate, Content Creator and ex-Professor with an interest in Data Science and Bioinformatics')
-
 
 st_button('youtube', 'https://youtube.com/dataprofessor', 'Data Professor YouTube channel', 26)
 st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel', 26)
