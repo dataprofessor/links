a02480d338de9d17d29d6ce6eb330c842d5632ec
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-06-29 09:51:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,10 +12,12 @@ st.header('Chanin Nantasenamat, Ph.D.')
 
 st.info('Developer Advocate, Content Creator and ex-Professor with an interest in Data Science and Bioinformatics')
 
-st_button('youtube', 'https://youtube.com/dataprofessor', 'Data Professor YouTube channel', 26)
-st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel', 26)
-st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs', 26)
-st_button('twitter', 'https://twitter.com/thedataprof/', 'Follow me on Twitter', 26)
-st_button('linkedin', 'https://www.linkedin.com/in/chanin-nantasenamat/', 'Follow me on LinkedIn', 26)
-st_button('newsletter', 'https://sendfox.com/dataprofessor/', 'Sign up for my Newsletter', 26)
-st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy me a Coffee', 26)
+button_font_size = 24
+
+st_button('youtube', 'https://youtube.com/dataprofessor', 'Data Professor YouTube channel', button_font_size)
+st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel', button_font_size)
+st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs', button_font_size)
+st_button('twitter', 'https://twitter.com/thedataprof/', 'Follow me on Twitter', button_font_size)
+st_button('linkedin', 'https://www.linkedin.com/in/chanin-nantasenamat/', 'Follow me on LinkedIn', button_font_size)
+st_button('newsletter', 'https://sendfox.com/dataprofessor/', 'Sign up for my Newsletter', button_font_size)
+st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy me a Coffee', button_font_size)
