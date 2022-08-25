908c68e016a446a1e00d5514a5305cefc68f9c46
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-08-25 14:20:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ from PIL import Image
 
 load_css()
 
+st.write("[![Star](https://img.shields.io/github/stars/dataprofessor/links.svg?logo=github&style=social)](https://gitHub.com/dataprofessor/links)")
+
 col1, col2, col3 = st.columns(3)
 col2.image(Image.open('dp.png'))
 
