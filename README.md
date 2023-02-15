d5944beb1d5741ce76e3689c0ffaaed23c5de2cb
Rakesh <rakesh.shaw018@gmail.com>
2023-02-15 12:20:55 +0530

    dark mode and three new icons added for Resume GitHub and Telegram.

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Welcome to Streamlit `links page`
 
-<img src="streamlit-links-page.png" width="350">
+<img src="./images/streamlit-links-page.png" width="350">
 
 
 > A Streamlit app that you can build for free to store all your personal links that is similar in functionality to that of [Linktr.ee](https://linktr.ee/).
 
-<img src="23F54497-245E-413F-99C7-F3E295E4EA13.png" width="350">
+<img src="./images/23F54497-245E-413F-99C7-F3E295E4EA13.png" width="350">
 
 # Demo app
 
--- st_functions.py
+++ st_functions.py
@@ -6,6 +6,8 @@ def load_css():
     st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 
 def st_button(icon, url, label, iconsize):
+
+# YouTube Button
     if icon == 'youtube':
         button_code = f'''
         <p>
@@ -16,6 +18,8 @@ def st_button(icon, url, label, iconsize):
                 {label}
             </a>
         </p>'''
+
+# Twitter Button
     elif icon == 'twitter':
         button_code = f'''
         <p>
@@ -26,6 +30,8 @@ def st_button(icon, url, label, iconsize):
             {label}
         </a>
         </p>'''
+
+# Linkedin Button
     elif icon == 'linkedin':
         button_code = f'''
         <p>
@@ -36,6 +42,8 @@ def st_button(icon, url, label, iconsize):
                 {label}
             </a>
         </p>''' 
+
+# Medium Button
     elif icon == 'medium':
         button_code = f'''
         <p>
@@ -46,6 +54,8 @@ def st_button(icon, url, label, iconsize):
                 {label}
             </a>
         </p>'''
+
+# Newsletter Button
     elif icon == 'newsletter':
         button_code = f'''
         <p>
@@ -56,6 +66,8 @@ def st_button(icon, url, label, iconsize):
                 {label}
             </a>
         </p>'''
+
+# Buy Me A Coffee Button
     elif icon == 'cup':
         button_code = f'''
         <p>
@@ -66,6 +78,43 @@ def st_button(icon, url, label, iconsize):
                 {label}
             </a>
         </p>'''
+
+# Telegram Button 
+    elif icon == 'telegram':
+        button_code = f'''
+        <p>
+            <a href={url} class="btn btn-outline-primary btn-lg btn-block" type="button" aria-pressed="true">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telegram" viewBox="0 0 16 16">
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.287 5.906c-.778.324-2.334.994-4.666 2.01-.378.15-.577.298-.595.442-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294.26.006.549-.1.868-.32 2.179-1.471 3.304-2.214 3.374-2.23.05-.012.12-.026.166.016.047.041.042.12.037.141-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8.154 8.154 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629.093.06.183.125.27.187.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.426 1.426 0 0 0-.013-.315.337.337 0 0 0-.114-.217.526.526 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09z"/>
+                </svg>
+                {label}
+            </a>
+        </p>'''
+
+# Resume button
+    elif icon == 'resume':
+        button_code = f'''
+        <p>
+            <a href={url} class="btn btn-outline-primary btn-lg btn-block" type="button" aria-pressed="true">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-person-fill" viewBox="0 0 16 16">
+                    <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm-1 7a3 3 0 1 1-6 0 3 3 0 0 1 6 0zm-3 4c2.623 0 4.146.826 5 1.755V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1v-1.245C3.854 11.825 5.377 11 8 11z"/>
+                </svg>
+                {label}
+            </a>
+        </p>'''
+
+# Github Button
+    elif icon == 'github':
+        button_code = f'''
+        <p>
+            <a href={url} class="btn btn-outline-primary btn-lg btn-block" type="button" aria-pressed="true">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                    <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+                </svg>
+                {label}
+            </a>
+        </p>'''
+
     elif icon == '':
         button_code = f'''
         <p>
